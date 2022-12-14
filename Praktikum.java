52f23a5db6779fb56a6a3d360d70e9f71f097024
Goryachaya.G <g.k.zaytseva@yandex.ru>
2022-12-14 21:06:31 +1100

    добавлен сканер

--- Praktikum.java
+++ Praktikum.java
@@ -1,4 +1,6 @@
 public class Praktikum {
 Public static void main (String[] args) {
+	Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
 }
 }
